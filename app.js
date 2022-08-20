8694612afe674eb00bb4cd38b8e5c7e4b6fc1c20
Lea Rosema <lea@lea.codes>
2022-08-20 10:12:44 +0200

    chore: work on app.js

--- app.js
+++ app.js
@@ -1,19 +1,61 @@
+import { vertexShader, fragmentShader } from './shaders';
+
 const {
-  WebGLRenderer,
+  Clock,
   Scene,
   Mesh,
   PlaneGeometry,
   ShaderMaterial,
   RenderTarget,
-  OrthoGraphicCamera,
+  OrthographicCamera,
+  WebGLRenderer,
 } = THREE;
 
 const $ = document.querySelector.bind(document);
 
 export class Application {
 
+  get pixelRatio() {
+    return Math.min(2, window.devicePixelRatio);
+  }
+
+  dimensions = {
+    get width() { return document.body.clientWidth; },
+    get height() { return document.body.clientHeight; },
+  }
+
   constructor() {
-    
+    this.canvas = $('.world');
+    this.renderer = new WebGLRenderer({canvas: this.canvas});
+    this.clock = new Clock();
+    this.camera = this.setupCamera();
+    this.scene = this.setupScene();
+  }
+  
+  setupCamera() {
+    const { width, height } = this.dimensions;
+    const camera = new OrthographicCamera(
+      -width / 2, width / 2,
+      -height / 2, height / 2,
+      1, 
+      100,
+    );
+    return camera;
+  }
+  
+  setupScene() {
+    const scene = new Scene();
+    const geometry = new PlaneGeometry(1, 1);
+    const material = new ShaderMaterial({
+      fragmentShader,
+      vertexShader,
+      uniforms: {
+        time: { value: 0 },
+      },
+    });
+    const mesh = new Mesh(geometry, material);
+    scene.add(mesh);
+    return scene;
   }
 
   onResize = () => {
