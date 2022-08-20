997f6d10ab1cc35622c35d7b08f60db77a723793
Lea Rosema <lea@lea.codes>
2022-08-20 16:05:32 +0200

    chore: make the code work

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { vertexShader, fragmentShader } from './shaders';
+import { vertexShader, fragmentShader } from "./shaders.js";
 
 const {
   Clock,
@@ -6,64 +6,93 @@ const {
   Mesh,
   PlaneGeometry,
   ShaderMaterial,
-  RenderTarget,
-  OrthographicCamera,
+  WebGLRenderTarget,
+  PerspectiveCamera,
   WebGLRenderer,
 } = THREE;
 
 const $ = document.querySelector.bind(document);
 
 export class Application {
+  currentPixelRatio = 1;
 
   get pixelRatio() {
     return Math.min(2, window.devicePixelRatio);
   }
 
   dimensions = {
-    get width() { return document.body.clientWidth; },
-    get height() { return document.body.clientHeight; },
-  }
+    get width() {
+      return document.body.clientWidth;
+    },
+    get height() {
+      return document.body.clientHeight;
+    },
+  };
 
   constructor() {
-    this.canvas = $('.world');
-    this.renderer = new WebGLRenderer({canvas: this.canvas});
+    this.canvas = $(".world");
+    this.renderer = new WebGLRenderer({ canvas: this.canvas });
     this.clock = new Clock();
     this.camera = this.setupCamera();
     this.scene = this.setupScene();
+
+    window.addEventListener("resize", this.onResize, false);
   }
-  
+
   setupCamera() {
     const { width, height } = this.dimensions;
-    const camera = new OrthographicCamera(
-      -width / 2, width / 2,
-      -height / 2, height / 2,
-      1, 
-      100,
-    );
+    const fieldOfView = 60;
+    const aspect = width / height;
+    const camera = new PerspectiveCamera(fieldOfView, aspect, 1, 100);
     return camera;
   }
-  
+
+  updateCamera() {
+    const { camera } = this;
+    const { width, height } = this.dimensions;
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+  }
+
   setupScene() {
     const scene = new Scene();
-    const geometry = new PlaneGeometry(1, 1);
+    const geometry = new PlaneGeometry(2, 2);
     const material = new ShaderMaterial({
       fragmentShader,
       vertexShader,
+      side: THREE.DoubleSide,
       uniforms: {
         time: { value: 0 },
       },
     });
     const mesh = new Mesh(geometry, material);
     scene.add(mesh);
+    scene.add(this.camera);
     return scene;
   }
 
   onResize = () => {
-  
-  }
+    const { renderer, dimensions } = this;
+    renderer.setSize(dimensions.width, dimensions.height);
+    this.camera.updateProjectionMatrix();
+  };
 
-  loop = () => {
-  
-  }
-
+  run = () => {
+    const { renderer, pixelRatio, scene, camera, clock } = this;
+    if (!this.currentPixelRatio !== pixelRatio) {
+      this.currentPixelRatio = pixelRatio;
+      renderer.setPixelRatio(pixelRatio);
+    }
+    for (const mesh of scene.children) {
+      if (mesh.type !== "Mesh" || mesh.material.type !== "ShaderMaterial") {
+        continue;
+      }
+      mesh.material.uniforms.time.value = clock.getElapsedTime();
+    }
+    renderer.render(scene, camera);
+    requestAnimationFrame(this.run);
+  };
 }
+
+const app = new Application();
+app.run();
--- shaders.js
+++ shaders.js
@@ -1,18 +1,23 @@
-const glsl = x => x[0].trim();
+const glsl = (x) => x[0].trim();
 
 export const vertexShader = glsl`
 varying vec2 vUv;
 
 void main() {
   vUv = uv; 
-  gl_Position = position;
+  gl_Position = vec4(position, 1.);
 }
 `;
 
 export const fragmentShader = glsl`
 varying vec2 vUv;
-
+uniform float time;
 void main() {
-  gl_FragColor = vec4(1., 0., 0., 1.);
+  gl_FragColor = vec4(
+    .5 + .5 * sin(time+vUv.x * 2.),
+    .5 + .5 * sin(time+vUv.y * 2.),
+    vUv.y,
+    1.
+  );
 }
 `;
